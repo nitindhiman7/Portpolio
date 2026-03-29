9a691a9e9cf6af3247faf80221ca3271535de126
Nitin Dhiman <nitindhiman86@gmail.com>
2026-03-29 16:02:17 +0530

    project images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,34 +1,41 @@
+import project1 from "../assets/images/project1.jpg";
+import project2 from "../assets/images/project2.jpg";
+import project3 from "../assets/images/project3.jpg";
+import project4 from "../assets/images/project4.jpg";
+import project5 from "../assets/images/project5.jpg";
+import project6 from "../assets/images/project6.jpg";
+
 function Projects() {
   const projects = [
     {
       title: "www.bentley.com",
       link: "https://www.bentley.com",
-      image: "/Portpolio/images/project1.jpg",
+      image: project1,
     },
     {
       title: "www.bentley.cn",
       link: "https://www.bentley.cn",
-      image: "/Portpolio/images/project2.jpg",
+      image: project2,
     },
     {
       title: "Origo",
       link: "https://www.origoinfotech.com",
-      image: "/Portpolio/images/project3.jpg",
+      image: project3,
     },
     {
       title: "Blog",
       link: "https://blog.bentley.com/",
-      image: "/Portpolio/images/project4.jpg",
+      image: project4,
     },
     {
       title: "Puprise",
       link: "https://www.puprise.com/",
-      image: "/Portpolio/images/project5.jpg",
+      image: project5,
     },
     {
       title: "Brainmine Tech",
       link: "https://www.brainminetech.com/",
-      image: "/Portpolio/images/project6.jpg",
+      image: project6,
       
     },
   ];
